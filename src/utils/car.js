5b84be9a80ac07212e15724430e5365f8d0076aa
nttu94507 <nttu94507@gmail.com>
2022-02-11 16:35:12 +0800

    jest mock spy

---
+++ src/utils/car.js
@@ -0,0 +1,16 @@
+import uuid from 'uuid/v1';
+
+const car = {
+
+  carContent: [],
+
+  getCurrentCar: () => ({car:car.carContent}),
+
+  addProdToCar: (name, count) => {
+    const workCar = [...car.getCurrentCar()];
+    workCar.push({ id: uuid(), name, count });
+    return workCar;
+  },
+};
+
+export default car;
