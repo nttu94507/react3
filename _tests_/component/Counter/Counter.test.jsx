f466e6f6018ff34a6052c66d612f5da72851d283
nttu94507 <nttu94507@gmail.com>
2022-02-23 18:39:20 +0800

    component jest

--- _tests_/component/Counter/Counter.test.jsx
+++ _tests_/component/Counter/Counter.test.jsx
@@ -1,6 +1,6 @@
 import '@testing-library/jest-dom'
 import React from "react";
-import { render } from "@testing-library/react";
+import { render,fireEvent} from "@testing-library/react";
 import { toBeInTheDocument } from "@testing-library/jest-dom";
 import Counter from "../../../src/component/Counter";
 
@@ -8,6 +8,9 @@ import Counter from "../../../src/component/Counter";
 describe(Counter,()=>{
     test('Counter_Click_AddCount',()=>{
         const{ getByTestId } = render(<Counter />);
-        expect (getByTestId('counterBlock')).toBeInTheDocument();
+        expect(getByTestId('counter').textContent).toBe('0')
+        const addCountBtn = getByTestId('addCountBtn')
+        fireEvent.click(addCountBtn);
+        expect(getByTestId('counter').textContent).toBe('1')
     })
 })
