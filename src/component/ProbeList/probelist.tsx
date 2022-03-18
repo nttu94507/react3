62f1d1bbbd7aed5b83a97456d73b50b626dfb685
nttu94507 <nttu94507@gmail.com>
2022-03-18 13:40:12 +0800

    fix typed-scss-loader 6.2.0

--- src/component/ProbeList/probelist.tsx
+++ src/component/ProbeList/probelist.tsx
@@ -2,6 +2,27 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { addProbe } from "../../action/probelist";
 
+const Probe_list=()=>{
+  const probes = useSelector(state => state.probelist)
+  return probes.map(task=>(
+    <div>
+      {task}
+      </div>
+  )
+
+  )
+}
+
+
+const ProbeList =()=>{
+  return(
+    <div>
+      <Probe_list/>
+    </div>
+  )
+}
+
+
 
 const main =()=>{
     const dispatch = useDispatch();
@@ -20,7 +41,9 @@ const main =()=>{
           {/* <input value={newprobe['probename']}  /> */}
           <button type="button" onClick={()=>{dispatch(addProbe(newprobe))}}>新增待辦事項</button>
         </div>
+        < ProbeList/>
     </div>
+  
     )
 }
 
