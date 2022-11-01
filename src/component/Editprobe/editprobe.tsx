3e7e191ff0dc0bdd981d644c91454b047a6abdf2
nttu94507 <nttu94507@gmail.com>
2022-11-01 18:33:08 +0800

    [M]editprobe done

--- src/component/Editprobe/editprobe.tsx
+++ src/component/Editprobe/editprobe.tsx
@@ -61,34 +61,46 @@ const Editprobe = () => {
 
   return (
     <div className={styles.editprobe}>
-      <div>
-        <input {...register('ProbeId',{required:false})} />
+      <div className={`${styles.editlist} ${styles.editprobefontsize}`}>
+        <p>新增 Probe</p>
       </div>
-      <div>
+      <div className={styles.editlist}>
+       {/* <div>Probe ID </div> */}
+        <input {...register('ProbeId',{required:false})} placeholder={"  請輸入Probe ID"}/>
+      </div>
+      <div className={styles.editlist}>
+        {/* <div>容量大小</div> */}
         <select {...register('Storage',{required:false})}>
           <option value={""}>請選擇容量大小</option>
           <option value={0}>8GB</option>
           <option value={1}>16GB</option>
         </select>
       </div>
-      <div>
+      <div className={styles.editlist}>
+        {/* <div>設備型號</div> */}
         <select {...register('Probetype',{required:false})}>
           <option value={""}>請選擇型號</option>
           <option value={0}>P110</option>
           <option value={1}>P120</option>
+          <option value={2}>P220</option>
+          <option value={3}>P360</option>
+          <option value={4}>P560</option>
+          <option value={4}>P110+(樹苺派4)</option>
         </select>
       </div>
-      <div>
-        <input {...register('note',{required:false})} />
+      <div className={styles.editlist}>
+        {/* <div>備註</div> */}
+        <input {...register('note',{required:false})} placeholder={"  請輸入備註"}/>
       </div>
-      
-      <button type="button" onClick ={()=>{
-        const value = getValues();
-        postcreateprobe(value,dispatch);
-        // console.log(value);
-        }} >
-        按
-      </button>
+      <div className={`${styles.editlist} `}>
+        <div className={styles.buttonDefault} onClick ={()=>{
+          const value = getValues();
+          postcreateprobe(value,dispatch);
+          // console.log(value);
+          }} >
+          確認送出
+        </div>
+      </div>
     </div>
     // <div >
     //   <div>probe ID : <input value={probe} onChange={(e) => { steNewprobe(e) }} /></div>
