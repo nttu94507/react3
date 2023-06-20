1db11f2ea2cece16975ec39c73bcf1977fc9ab93
yuchih.fu <nttu94507@gmail.com>
2023-06-20 11:21:35 +0800

    [M]add customer component

---
+++ src/component/Customer/Customer.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import styles from "./index.scss"
+
+const Customer = () => {
+    return(
+       <div className={styles.customer}>
+        <div className={styles.customerlist}></div>
+       </div> 
+    )
+}
+
+export default Customer
---
+++ src/component/Customer/index.scss.d.ts
@@ -0,0 +1,2 @@
+export const customer: string;
+export const customerlist: string;
