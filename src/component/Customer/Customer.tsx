64f52ac28bf7a2266198aad302a136b1c199add2
yuchih.fu <nttu94507@gmail.com>
2023-06-27 11:38:25 +0800

    [M] midified customer CSS

--- src/component/Customer/Customer.tsx
+++ src/component/Customer/Customer.tsx
@@ -1,11 +1,142 @@
 import React from "react"
 import styles from "./index.scss"
+import { useDispatch } from "react-redux"
+import { symbol } from "prop-types";
+
 
 const Customer = () => {
-    return(
-       <div className={styles.customer}>
-        <div className={styles.customerlist}></div>
-       </div> 
+    const dispatch = useDispatch();
+    return (
+        <div className={styles.customer}>
+            <div className={styles.customeroption}>
+                <div className={styles.item6}>
+                    <input placeholder={"  搜尋客戶"} className={styles.searchBar} onChange={(e) => dispatch(searchProbeDataBegin(e.target.value))}></input>
+                </div>
+                <div className={styles.item3}>
+
+                </div>
+                <div className={styles.item3}>
+                    <div onClick={() => addProbedialog(display)} className={styles.buttonDefault}>新增客戶 </div>
+                </div>
+            </div>
+            <div className={styles.customercontent}>
+                <div className={styles.customerLists}>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles.cardupper}>
+                            <div className={styles.carduppertittle}>松下產業</div>
+                            <div className={styles.cardupperstatus}>
+                                <div className={styles.cardupperstatusborder}>保固中</div>
+                            </div>
+                        </div>
+                        <div className={styles.cardbuttom}>
+                            <div className={styles.cardbuttomitem}>到期日: 2024/06/15</div>
+                            <div className={styles.cardbuttomitem}>承辦: 朝壽</div>
+                            <div className={styles.cardbuttomitem}>業務: 禎琳</div>
+                            <div className={styles.cardbuttomitem}>FAE: Jason</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
 }
 
--- src/component/Main/main.tsx
+++ src/component/Main/main.tsx
@@ -8,6 +8,7 @@ import Editprobe from '../Editprobe';
 import Dashboard from '../Dashboard';
 import Probe from '../Probe';
 import DeviceList from '../DeviceList/DeviceList';
+import Customer from '../Customer';
 
 const Main = () => (
   <div className={styles.main}>
@@ -15,7 +16,7 @@ const Main = () => (
       <div className={styles.sidemenu}>
       <Link to="/" data-testid="homeLink"><div className={styles.sidemenuBtn}>首頁</div></Link>
       <Link to="/probelist" data-testid="probelistLink"><div className={styles.sidemenuBtn}>Probe 管理</div></Link>
-      <Link to="/devicelist" data-testid="DevicelistLink"><div className={styles.sidemenuBtn}>Device 管理</div></Link>
+      <Link to="/customer" data-testid="customer"><div className={styles.sidemenuBtn}>客戶管理</div></Link>
       </div>
     </div>
     <div className={styles.right}>
@@ -24,7 +25,7 @@ const Main = () => (
         <Route path="/probelist" component={ProbeList} />
         <Route path="/detail/:Id" component={Probe} />
         <Route path="/editprobe" component={Editprobe} />
-        <Route path="/devicelist" component={DeviceList} />
+        <Route path="/customer" component={Customer} />
         <Route path="/list/:taskName" component={List} />
       </Switch>
     </div>
